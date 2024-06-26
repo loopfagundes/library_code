fac1503f113f9c3dd4f8da0aac3d49ad351ace2b
loopfagundes <lof.fagundes@gmail.com>
2024-06-26 15:56:21 -0300

    updt/add: Logger e JOptionPane

---
+++ src/main/java/br/dev/codex/JOptionPane/ExemploJOptionPane.java
@@ -0,0 +1,14 @@
+package br.dev.codex.JOptionPane;
+
+import javax.swing.*;
+
+public class ExemploJOptionPane {
+    /*
+     * Vai abrir automaticamente de uma mensagem da janela
+     */
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Mensagem em um diálogo");
+        JOptionPane.showInternalConfirmDialog(null, "Mensagem em um confirmar Sim ou Nao");
+    }
+}
--- src/main/java/br/dev/codex/logger/ExemploLogger.java
+++ src/main/java/br/dev/codex/logger/ExemploLogger.java
@@ -1,2 +1,14 @@
-package br.dev.codex.logger;public class ExemploLogger {
+package br.dev.codex.logger;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class ExemploLogger {
+//    private static final Logger logger = Logger.getLogger(ExemploLogger.class.getName());
+//
+//    public static void main(String[] args) {
+//        logger.log(Level.INFO, "Esta é uma mensagem de informação");
+//        logger.log(Level.WARNING, "Esta é uma mensagem de aviso");
+//        logger.log(Level.SEVERE, "Esta é uma mensagem de erro");
+//    }
 }
